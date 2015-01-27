32ab367cfc95bd638050cfd0b89ed91a5c347200
gcorradini <gregcorradini@gmail.com>
2015-01-27 15:45:51 -0800

    update example 3

--- src/main_ex_3_ractify.js
+++ src/main_ex_3_ractify.js
@@ -1,16 +1,4 @@
 var ex_3 = require('./ex_3.js')
 
 App = window.App || {};
-App.load_stream = ex_3;
-
-/*
-(function( def ) { // execute immiedately
-
-    App = def(); // return context hash
-
-})( function() {  // definition
-
-    return context;
-
-});
-*/
+App.load_demo = ex_3;
