2fded381ef1ea4a6b6d57849944c06f7ef236b10
gcorradini <gregcorradini@gmail.com>
2015-01-27 15:46:06 -0800

    create a build script for npm

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for file in ./src/main_*.js; do
+    filename=$(basename $file)
+    name="${filename%.*}"
+    if [[ $file == *"ractify"* ]]; then
+        echo "Transforming ractive dependent files..."
+        echo $file
+        ./node_modules/.bin/browserify -t ractify $(pwd)/src/$filename > $(pwd)/dist/$name'.js'
+    else
+        echo "Browserifying files..."
+        echo $file
+        ./node_modules/.bin/browserify $(pwd)/src/$filename > $(pwd)/dist/$name'.js'
+    fi
+done
